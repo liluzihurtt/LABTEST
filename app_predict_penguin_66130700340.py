0dda6536163759c0261a9d8a47eb2dc7ffc75d1b
liluzihurtt <157356428+liluzihurtt@users.noreply.github.com>
2024-11-10 15:30:17 +0700

    Update app_predict_penguin_66130700340.py

--- app_predict_penguin_66130700340.py
+++ app_predict_penguin_66130700340.py
@@ -1,35 +1,32 @@
-import streamlit as st
-import pandas as pd
 import pickle
-import sklearn.preprocessing
+import pandas as pd
+import streamlit as st
 from sklearn.preprocessing import LabelEncoder
-
-# Load the trained model pipeline and encoders
-try:
-    with open('model_penguin_66130700340.pkl', 'rb') as file:
-        model, species_encoder,island_encoder, sex_encoder = pickle.load(file)
-    st.write("Model and encoders loaded successfully!")
-except Exception as e:
-    st.write("Error loading model and encoders:", e)
-
-# Streamlit UI elements
-st.title("Penguin Species Prediction")
-
-# Input fields
-island = st.selectbox("Island", ["Biscoe", "Dream", "Torgersen"])
-culmen_length_mm = st.number_input("Culmen Length (mm)", min_value=0.0)
-culmen_depth_mm = st.number_input("Culmen Depth (mm)", min_value=0.0)
-flipper_length_mm = st.number_input("Flipper Length (mm)", min_value=0.0)
-body_mass_g = st.number_input("Body Mass (g)", min_value=0.0)
-sex = st.selectbox("Sex", ["MALE", "FEMALE"])
-
-x_new =  pd.DataFrame()
+ 
+# Load the trained model and encoders
+with open('model_penguin_66130700340.pkl', 'rb') as file:
+    model, species_encoder, island_encoder, sex_encoder = pickle.load(file)
+ 
+# Title of the app
+st.title('Predict Randomforest Penguin App')
+ 
+# Input fields for user to enter data
+island = st.selectbox('Select island:', ['Torgersen', 'Biscoe', 'Dream'])
+culmen_length_mm = st.number_input('Enter Culmen Length (mm):')
+culmen_depth_mm = st.number_input('Enter Culmen Depth (mm):')
+flipper_length_mm = st.number_input('Enter Flipper Length (mm):')
+body_mass_g = st.number_input('Enter Body Mass (g):')
+sex = st.selectbox('Select sex:', ['MALE', 'FEMALE'])
+ 
+# Create a DataFrame from user input
+x_new = pd.DataFrame()
 x_new['island'] = [island]
-x_new['culmen_length_mm'] = [culmen_length_mm]
-x_new['culmen_depth_mm'] = [culmen_depth_mm]
-x_new['flipper_length_mm'] = [flipper_length_mm]
-x_new['body_mass_g'] = [body_mass_g]
-x_new['sex'] = [sex]
+x_new['culmen_length_mm']= [culmen_length_mm]
+x_new['culmen_depth_mm']= [culmen_depth_mm]
+x_new['flipper_length_mm']= [flipper_length_mm]
+x_new['body_mass_g']= [body_mass_g]
+x_new['sex']= [sex]
+ 
  
 # Transform the input data using the encoders
 x_new['island'] = island_encoder.transform(x_new['island'])
