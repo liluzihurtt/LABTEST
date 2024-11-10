9a54b36daf56fd709e13e6256bb6d5ee00161a61
liluzihurtt <157356428+liluzihurtt@users.noreply.github.com>
2024-11-10 14:19:59 +0700

    Update app_predict_penguin_66130700340.py

--- app_predict_penguin_66130700340.py
+++ app_predict_penguin_66130700340.py
@@ -1,6 +1,15 @@
+%%writefile app_predict_penguin_66130700340.py
 import streamlit as st
 import pandas as pd
 import pickle
+
+# Load the trained model pipeline and encoders
+try:
+    with open('model_penguin_66130700340.pkl', 'rb') as file:
+        model_pipeline, species_encoder = pickle.load(file)
+    st.write("Model and encoders loaded successfully!")
+except Exception as e:
+    st.write("Error loading model and encoders:", e)
 
 # Streamlit UI elements
 st.title("Penguin Species Prediction")
@@ -14,18 +23,28 @@ body_mass_g = st.number_input("Body Mass (g)", min_value=0.0)
 sex = st.selectbox("Sex", ["MALE", "FEMALE"])
 
 # Create a DataFrame for the input data
-input_data = pd.DataFrame({
-    'island': [island],
-    'culmen_length_mm': [culmen_length_mm],
-    'culmen_depth_mm': [culmen_depth_mm],
-    'flipper_length_mm': [flipper_length_mm],
-    'body_mass_g': [body_mass_g],
-    'sex': [sex]
-})
+# input_data = pd.DataFrame({
+#     'island': [island],
+#     'culmen_length_mm': [culmen_length_mm],
+#     'culmen_depth_mm': [culmen_depth_mm],
+#     'flipper_length_mm': [flipper_length_mm],
+#     'body_mass_g': [body_mass_g],
+#     'sex': [sex]
+# })
+x_new =  pd.DataFrame() 
+x_new['island'] = [island]
+x_new['culmen_length_mm'] = [culmen_length_mm]
+x_new['culmen_depth_mm'] = [culmen_depth_mm]
+x_new['flipper_length_mm'] = [flipper_length_mm]
+x_new['body_mass_g'] = [body_mass_g]
+x_new['sex'] = [sex]
+
+x_new['island'] = island_encoder.transform(x_new['island'])
+x_new['sex'] = sex_encoder.transform(x_new['sex'])
 
 # Make predictions
 try:
-    prediction = model_pipeline.predict(input_data)
+    prediction = model_pipeline.predict(x_new)
     predicted_species = species_encoder.inverse_transform(prediction)
     st.write("Predicted Species:", predicted_species[0])
 except Exception as e:
