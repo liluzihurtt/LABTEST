c0406d072bea4efa502b30ed657a489df5023b23
liluzihurtt <157356428+liluzihurtt@users.noreply.github.com>
2024-11-10 15:25:23 +0700

    Update app_predict_penguin_66130700340.py

--- app_predict_penguin_66130700340.py
+++ app_predict_penguin_66130700340.py
@@ -23,23 +23,23 @@ flipper_length_mm = st.number_input("Flipper Length (mm)", min_value=0.0)
 body_mass_g = st.number_input("Body Mass (g)", min_value=0.0)
 sex = st.selectbox("Sex", ["MALE", "FEMALE"])
 
-
-x_new =  pd.DataFrame() 
+x_new =  pd.DataFrame()
 x_new['island'] = [island]
 x_new['culmen_length_mm'] = [culmen_length_mm]
 x_new['culmen_depth_mm'] = [culmen_depth_mm]
 x_new['flipper_length_mm'] = [flipper_length_mm]
 x_new['body_mass_g'] = [body_mass_g]
 x_new['sex'] = [sex]
-
+ 
+# Transform the input data using the encoders
 x_new['island'] = island_encoder.transform(x_new['island'])
 x_new['sex'] = sex_encoder.transform(x_new['sex'])
-
-
+ 
 # Make predictions
-try:
-    prediction = model_pipeline.predict(x_new)
-    predicted_species = species_encoder.inverse_transform(prediction)
-    st.write("Predicted Species:", predicted_species[0])
-except Exception as e:
-    st.write("Error during prediction:", e)
+y_pred_new = model.predict(x_new)
+ 
+# Decode the predicted species
+result = species_encoder.inverse_transform(y_pred_new)
+ 
+# Display the prediction
+st.write('Predicted Species:', result[0])
